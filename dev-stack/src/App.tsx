a6101df7fccc3cc723d2366af4824b302e4afa6e
tniha10 <tahsinamamun10@gmail.com>
2026-09-11 16:45:26 +0600

    mobile navbar

--- dev-stack/src/App.tsx
+++ dev-stack/src/App.tsx
@@ -1,12 +1,9 @@
 import Nav from "./components/Nav"; 
 
 function App() {
-  
-
   return (
     <>
       <Nav />
-      
     </>
   )
 }
--- dev-stack/src/components/Nav.tsx
+++ dev-stack/src/components/Nav.tsx
@@ -1,11 +1,32 @@
 import Logo from "../assets/logo-text.png";
+import Icon from "../assets/hamburger-icon.png";
 
 const Nav = () => {
     return (
-        <nav className="sticky flex items-center justify-between bg-white border-b border-gray-100 px-16 py-4"> {}
-           <div className="flex items-center"> <img src={Logo} alt="Logo" /></div>
+        <nav className="sticky flex items-center justify-between bg-white border-b border-gray-100 px-4 py-4 md:px-16"> {/*sticky is used so that the navbar stay put and md - show this normally but hide it when the screen reaches this.*/}
 
-        <div className="flex items-center gap-8">
+        {/*Mobile menu button*/}
+           <div className="flex items-center justify-between w-full md:hidden"> 
+                  
+                  <div>
+                    <img src={Icon} alt="Menu" />
+                  </div>
+
+                  <div>
+                    <img src={Logo} alt="Logo" />
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <button className=" text-[#334155] text-xs font-medium">Sign In</button>
+                    <button className=" bg-[#D91B7E] text-white text-xs font-medium py-2 px-3 rounded-full">Sign Up</button>
+                  </div>
+            </div>    
+
+           
+        {/*Desktop menu*/}
+        <div className="hidden md:flex items-center"><img src={Logo} alt="Logo" /></div>
+
+        <div className="hidden md:flex items-center gap-8"> {/* hidden md: flex - hide it on small screens but show it as flex from md and above*/}
             <a href="" className="text-[#DB2777] text-sm font-medium">Home</a>
             <a href="" className="text-[#475569] text-sm font-medium">Technologies</a>
             <a href="" className="text-[#475569] text-sm font-medium">Projects</a>
@@ -13,8 +34,8 @@ const Nav = () => {
             <a href="" className="text-[#475569] text-sm font-medium">Contact</a>
         </div>
 
-        <div className="flex items-center gap-4">
-            <button className=" text-[334155] text-sm font-medium">Sign In</button>
+        <div className="hidden md:flex items-center gap-4">
+            <button className=" text-[#334155] text-sm font-medium">Sign In</button>
             <button className="bg-[#D91B7E] text-white text-sm font-medium py-2 px-4 rounded-full">Sign Up</button>
         </div>
 
